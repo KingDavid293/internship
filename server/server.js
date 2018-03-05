0316994af936f3f604d52faa8f903b85bfccd405
Arun <davidtheking293@gmail.com>
2018-03-05 22:45:12 +0530

    my commit 1

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var BodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/user-info');
 
-var User = require('./model/user');
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -63,84 +63,6 @@ app.post('/signup',function(req,res){
     }
   });
 });
-
-
-app.post('/login',function(req,res){
-  var user = new User();
-  user.Password = req.body.Password;
-  user.eMail = req.body.eMail;
-
- var passMatch = false;
- var noEmail = true;
- var j;
-
-
-  User.find({},function(err, users){
-    if(err){
-      res.status(500).send({error:"some error occured"});
-    }else {
-      for(var i = 0 ; i < users.length ;  i++){
-        if(users[i].eMail == user.eMail){
-          noEmail = false;
-          if(users[i].Password == user.Password){
-            passMatch = true;
-            j = i;
-            break;
-          }
-        }
-      }
-      if(passMatch){
-        res.status(200).send(users[j]);
-      } else {
-        if(noEmail){
-          res.status(200).send("noEmail");
-        }else {
-          res.status(200).send('{"message":"passwordnotmatched"}');
-        }
-      }
-    }
-  });
-});
-
-
-
-app.post('/edit',function(req,res){
-  //
-
-  var user = new User();
-
-  user.Name = req.body.Name;
-  user.PhoneNo = req.body.PhoneNo;
-  user.aboutMe = req.body.aboutMe;
-  user.sex = req.body.sex;
-  user.hobies = req.body.hobies;
-  user._id = req.body._id;
-  //
-//   User.findByIdAndUpdate( req.body.id, { $set: { Name : req.body.Name , PhoneNo : req.body.PhoneNo , aboutMe : req.body.aboutMe , sex : req.body.sex , hobies : req.body.hobies }} , { new : true }, function (err, user) {
-//   if (err) res.send("error occored");
-//   console.log(User);
-//   res.send(user);
-// });
-
-  User.findById(user._id,function(err, user){
-    if(err){
-      res.status(500).send({error:"some error occured"});
-    } else {
-       User().save(function(err, user){
-         if (err) {
-           res.send("errer occured");
-         } else {
-            res.send(user);
-         }
-         });
-    }
-
-  });
-});
-
-
-
-
 
 app.listen(3000, function(){
   console.log('server is running on port 3000');
